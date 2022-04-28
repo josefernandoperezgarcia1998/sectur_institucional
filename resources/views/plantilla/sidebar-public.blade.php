b641fe69f58ab7cfa5a32854b778a2dffffd351b
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-04-28 11:29:59 -0500

    I create the home page, sidebar and header

---
+++ resources/views/plantilla/sidebar-public.blade.php
@@ -0,0 +1,121 @@
+<ul class="list-unstyled ps-0">
+    <li class="mb-1">
+        <a  href="" class="btn align-items-center rounded collapsed">
+            Inicio
+        </a>
+    </li>
+    <li class="mb-1">
+        <a href="" class="btn align-items-center rounded collapsed">
+            Conócenos
+        </a>
+    </li>
+    <li class="mb-1">
+        <a href="" class="btn align-items-center rounded collapsed">
+            Marco Jurídico
+        </a>
+    </li>
+    <li class="mb-1">
+        <a href="" class="btn align-items-center rounded collapsed">
+            Servicios
+        </a>
+    </li>
+    <li class="mb-1">
+        <a href="" class="btn align-items-center rounded collapsed">
+            Transparencia
+        </a>
+    </li>
+    <li class="mb-1">
+        <a href="" class="btn align-items-center rounded collapsed">
+            Datos Personales
+        </a>
+    </li>
+
+
+
+
+
+{{-- 
+
+    <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed"
+            data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+            Orders
+        </button>
+        <div class="collapse" id="orders-collapse">
+            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                <li><a href="#" class="link-dark rounded">New</a></li>
+                <li><a href="#" class="link-dark rounded">Processed</a></li>
+                <li><a href="#" class="link-dark rounded">Shipped</a></li>
+                <li><a href="#" class="link-dark rounded">Returned</a></li>
+            </ul>
+        </div>
+    </li>
+
+
+
+
+
+
+    
+    <li class="border-top my-3"></li>
+    <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed"
+            data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+            Account
+        </button>
+        <div class="collapse" id="account-collapse">
+            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                <li><a href="#" class="link-dark rounded">New...</a></li>
+                <li><a href="#" class="link-dark rounded">Profile</a></li>
+                <li><a href="#" class="link-dark rounded">Settings</a></li>
+                <li><a href="#" class="link-dark rounded">Sign out</a></li>
+            </ul>
+        </div>
+    </li>
+     --}}
+</ul>
+{{--                         
+                        <li class="mb-1">
+                            <button class="btn btn-toggle align-items-center rounded collapsed"
+                                data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+                                Dashboard
+                            </button>
+                            <div class="collapse" id="dashboard-collapse">
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <li><a href="#" class="link-dark rounded">Overview</a></li>
+                                    <li><a href="#" class="link-dark rounded">Weekly</a></li>
+                                    <li><a href="#" class="link-dark rounded">Monthly</a></li>
+                                    <li><a href="#" class="link-dark rounded">Annually</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="mb-1">
+                            <button class="btn btn-toggle align-items-center rounded collapsed"
+                                data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+                                Orders
+                            </button>
+                            <div class="collapse" id="orders-collapse">
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <li><a href="#" class="link-dark rounded">New</a></li>
+                                    <li><a href="#" class="link-dark rounded">Processed</a></li>
+                                    <li><a href="#" class="link-dark rounded">Shipped</a></li>
+                                    <li><a href="#" class="link-dark rounded">Returned</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="border-top my-3"></li>
+                        <li class="mb-1">
+                            <button class="btn btn-toggle align-items-center rounded collapsed"
+                                data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+                                Account
+                            </button>
+                            <div class="collapse" id="account-collapse">
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <li><a href="#" class="link-dark rounded">New...</a></li>
+                                    <li><a href="#" class="link-dark rounded">Profile</a></li>
+                                    <li><a href="#" class="link-dark rounded">Settings</a></li>
+                                    <li><a href="#" class="link-dark rounded">Sign out</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        --}}
